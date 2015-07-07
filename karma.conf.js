943ace894a00f6e59374948e5a8070cbf17c155d
Lucas Myers <lucas@unthunk.com>
2015-07-07 01:26:13 -0400

    adding karma and jasmine for testing

---
+++ karma.conf.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = function(config){
+  config.set({
+
+    basePath : './',
+
+    files : [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular-route/angular-route.js',
+      'www/app/app.js',
+      'www/app/views/**/*.js',
+      'www/app/services/**/*.js',
+      'test/unit/**/*.spec.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome', 'Firefox'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
