ec7ee6586d6d6768aae00287704c3ce8d2c274a8
Lucas Myers <lucas@unthunk.com>
2015-07-17 02:52:08 -0400

    added dev dependencies for build process, configure build stesp in gruntfile, updated karma, updated README

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,96 @@ module.exports = function (grunt) {
             ]
         }
       }
+    },
+
+    // clean up build folders
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            'dist/*'
+          ]
+        }]
+      }
+    },
+
+    // minify css
+    cssmin: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'dist/css/app.min.css' : [
+            'bower_components/skeleton-css/css/normalize.css',
+            'bower_components/skeleton-css/css/skeleton.css',
+            'www/css/app.css'
+          ]
+        }
+      }
+    },
+
+    // process index.html file for dist
+    processhtml: {
+      dist: {
+        files: {
+          'dist/index.html': ['www/index.html']
+        }
+      }
+    },
+
+    // lint the js for issues
+    jshint: {
+      all: [ 'www/app/**/*.js' ]
+    },
+
+    // combine the js files
+    concat: {
+      dist: {
+        src: [
+          'bower_components/angular/angular.min.js',
+          'bower_components/angular-route/angular-route.min.js',
+          'bower_components/angular-resource/angular-resource.min.js',
+          'www/app/app.js',
+          'www/app/**/*.js'
+        ],
+        dest: 'dist/js/app.js',
+      }
+    },
+
+    // minify the js
+    uglify: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'dist/js/app.min.js': [ 'dist/js/app.js' ]
+        }
+      }
+    },
+
+    // copy over otherwise unhandled files
+    copy: {
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: 'www/app',
+            src: ['**/*.html'],
+            dest: 'dist/app'
+          }
+        ]
+      }
+    },
+
+    // karma config
+    karma: {
+      options: {
+        configFile: 'config/karma.conf.js'
+      }
     }
 
   });
@@ -34,6 +124,15 @@ grunt.registerTask('test', [
 
 grunt.registerTask('server', [
   'nodemon'
+]);
+
+grunt.registerTask('dist',[
+  'clean:dist',
+  'cssmin:dist',
+  'concat:dist',
+  'uglify:dist',
+  'copy:dist',
+  'processhtml:dist'
 ]);
 
 };
--- README.md
+++ README.md
@@ -1,22 +1,43 @@
 # git-stars
 
-Display git projects for an organization ranked by number of stargazers and allow browsing of recent commits.
+Display git projects for an organization ranked by number of stargazers and allows browsing of recent commits.
 
 ## Installing
 
-`npm install`
+```
+npm install
+```
 
-`bower install`
+```
+bower install
+```
 
-Edit www/app/views/main/main.js to set $scope.org to the github org of your choice.
-Optionally change the value for $scope.days in /www/app/views/repo/repo.js to change how many days of commits to search for.
+- Edit www/app/views/main/main.js to set $scope.org to the github org of your choice.
+- Optionally change the value for $scope.days in /www/app/views/repo/repo.js to change how many days of commits to search for (90 by default).
 
 ## Running
 
-`grunt`
+```
+grunt
+```
 
 This will start an express server with nodemon watching /www and /bower_components on http://localhost:3000/
 
+## Building
+
+```
+grunt dist
+```
+Handles minificiation, concatenation, and uglification, and prepares the app for distribution.  Everything is bundled up in /dist.
+
+After building, you can start express against the /dist folder with:
+
+```
+ENV=\dist nodemon
+```
+
 ## Running tests
 
-`npm test`
+```
+npm test
+```
